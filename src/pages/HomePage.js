505db0b0d3a3785f007c55bb09101b98cee0a12a
barandiloglu <barandiloglu@gmail.com>
2024-04-15 08:57:22 -0400

    Layout

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,7 +12,7 @@ function HomePage() {
     useEffect(() => {
         const interval = setInterval(() => {
           setIsRotated(true);
-          setRotationDegrees((prevDegrees) => prevDegrees + 90);
+          setRotationDegrees((prevDegrees) => prevDegrees - 90);
           setTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
           setTimeout(() => {
             setIsRotated(false);
@@ -39,38 +39,38 @@ function HomePage() {
   return (
     <div className='flex flex-col w-full min-h-screen bg-primary'>
         <div className='flex flex-col items-center w-full mt-20 space-y-8'>
-            <h1 className='text-7xl font-lemon-milk text-third'>
+            <h1 className='2xl:text-7xl min-[320px]:text-5xl font-lemon-milk text-third'>
                 WE CREATE
             </h1>
 
-            <h1 className='relative text-7xl font-lemon-milk text-secondary'>
+            <h1 className='2xl:text-7xl min-[320px]:text-5xl font-lemon-milk text-secondary'>
                 THE CONTENT
             </h1>
         </div>
 
         <div className='flex w-full mt-10 bg-secondary'>
-            <div className='flex flex-row'>
+            <div className='flex flex-row w-full'>
                 <img
-                    className={`w-[30%] h-auto object-cover`}
+                    className={`2xl:w-[30%] xl:w-[30%] lg:w-[30%] md:w-[30%] sm:w-[25%] min-[320px]:w-[20%] object-cover z-10`}
                     src={LogoBlackWhite}
                     alt='Logo-BlackWhite'
                     style={{
-                    marginLeft: '-15%',
-                    transform: `rotate(${rotationDegrees}deg)`,
-                    transition: isRotated ? 'transform 0.5s ease' : 'none',
+                        marginLeft: '-15%',
+                        transform: `rotate(${rotationDegrees}deg)`,
+                        transition: isRotated ? 'transform 0.5s ease' : 'none',
                     }}
                 />
 
-                <div className='flex flex-col justify-between xl:ml-0 sm:ml-4'>
-                    <h1 className={`mt-4 xl:text-2xl sm:text-xl text-third font-lemon-milk ${isRotated ? 'animate-slide-up-1' : ''}`}>
+                <div className='flex w-full flex-col justify-between bg-transparent'>
+                    <h1 className={`mt-4 xl:text-2xl sm:text-xl min-[320px]:text-md text-third font-lemon-milk ${isRotated ? 'animate-slide-up-1' : ''}`}>
                         {texts[textIndex].title}
                     </h1>
 
-                    <p className='xl:text-xl sm:text-md text-justify mr-[10%] xl:mt-2 sm:mt-4 text-third font-alata'>
+                    <p className='xl:text-xl sm:text-md min-[320px]:text-xs text-justify mr-[10%] xl:mt-2 sm:mt-4 text-third font-alata'>
                         {texts[textIndex].content}
                     </p>
 
-                    <p className='mt-4 mb-4 underline xl:text-xl sm:text-md text-third font-alata'>
+                    <p className='mt-4 mb-4 underline xl:text-xl sm:text-md min-[320px]:text-xs text-third font-alata'>
                         {texts[textIndex].button}
                     </p>
                 </div>
